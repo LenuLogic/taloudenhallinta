cb7a4af2d421e0bb03fa947d7d61734cf4f6fdc7
Leena koo <leena.kevatkyla@gmail.com>
2025-01-08 10:22:02 +0200

    Lisätään Item-komponentti

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
+import './App.css';
+import Item from '../Item';
 
 function App() {
 
   return (
     <>
       <div>
-        TODO projekti
+        <Item />
       </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,18 @@
+import styles from './Item.module.scss';
+
+function Item() {
+    return (
+        <div className={styles.item}> {/*block-element-modifier-nimeämiskäytäntö*/}
+            <div className={styles.item_data}>
+                <div className={styles.item_type}>Sähkö</div>
+                <div className={styles.item_amount}>185,50 €</div>
+                <div className={styles.item_date}>1.2.2025</div>
+                <div className={styles.item_timespan}>1.10.2024 – 1.1.2025</div>
+                <div className={styles.item_receiver}>Helen Oy</div>
+                <div className={styles.item_average}>61,84 €/kk</div>
+            </div>
+        </div>
+    )
+}
+
+export default Item
