393b72d68006452428e4d8ce8ccf6b33fee6e9d9
Leena koo <leena.kevatkyla@gmail.com>
2025-01-08 12:08:22 +0200

    Menu-komponentin ja tyylimäärittelyiden lisääminen

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import styles from './Menu.module.scss';
+
+function Menu() {
+    return (
+        <div className={styles.menu}>
+            <div>list</div>
+            <div>stats</div>
+            <div>profile</div>
+        </div>
+    );
+}
+
+export default Menu;
