6eb6755539be92c353dbf926f893167f3945dd40
Leena koo <leena.kevatkyla@gmail.com>
2025-01-22 12:55:19 +0200

    Lisätään kulutyypin lisäys.

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -19,7 +19,8 @@ function AppRouter (props) {
                 loader: () => { return props.data } },
                 { path: "stats", element: <Stats /> },
                 { path: "settings", 
-                element: <Settings typelist={props.typelist} /> },
+                element: <Settings typelist={props.typelist} 
+                                    onTypeSubmit={props.onTypeSubmit} /> },
                 { path: "add", 
                 element: <AddItem   onItemSubmit={props.onItemSubmit} 
                                     typelist={props.typelist} /> },
